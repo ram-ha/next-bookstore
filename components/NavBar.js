442354b5e2b6943b7e6d7ea8e9c708fd6f9c6a68
hram <dlgkfka2616@gmail.com>
2023-10-16 02:01:06 +0900

    in macbook css 조정

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,46 +1,46 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function NavBar() {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <nav>
-            <div className="linkWrap">
-                <Link href="/">
-                    <a className={router.pathname === '/' ? 'active' : ''}>HOME</a>
-                </Link>
-                <Link href="/about">
-                    <a className={router.pathname === '/about' ? 'active' : ''}>ABOUT</a>
-                </Link>
-            </div>
-            <style jsx>
-                {`
-                    nav {
-                        border-bottom: 1px solid #449745;
-                        height: 60px;
-                    }
-                    .linkWrap {
-                        margin: 0 auto;
-                        display: flex;
-                        align-items: center;
-                        justify-content: flex-end;
-                        width: 650px;
-                    }
-                    nav a {
-                        font-weight: 600;
-                        font-size: 18px;
-                        padding: 18px 10px;
-                        border-left: 1px solid #449745;
-                        &:nth-child(2) {
-                            border-right: 1px solid #449745;
-                        }
-                    }
-                    .active {
-                        color: tomato;
-                    }
-                `}
-            </style>
-        </nav>
-    );
+  return (
+    <nav>
+      <div className="linkWrap">
+        <Link href="/">
+          <a className={router.pathname === "/" ? "active" : ""}>HOME</a>
+        </Link>
+        <Link href="/about">
+          <a className={router.pathname === "/about" ? "active" : ""}>ABOUT</a>
+        </Link>
+      </div>
+      <style jsx>
+        {`
+          nav {
+            border-bottom: 1px solid #449745;
+            height: 60px;
+          }
+          .linkWrap {
+            margin: 0 auto;
+            display: flex;
+            align-items: center;
+            justify-content: flex-end;
+            width: 650px;
+          }
+          nav a {
+            font-weight: 600;
+            font-size: 18px;
+            padding: 19px 10px;
+            border-left: 1px solid #449745;
+            &:nth-child(2) {
+              border-right: 1px solid #449745;
+            }
+          }
+          .active {
+            color: tomato;
+          }
+        `}
+      </style>
+    </nav>
+  );
 }
--- pages/list/[id].js
+++ pages/list/[id].js
@@ -1,126 +1,129 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function bestsellers({ bookList }) {
-    const { books } = bookList;
-    return (
-        <div className="bookList">
-            <div className="title">{bookList.display_name} BOOKS</div>
-            <div className="booksWrap">
-                {books.map((book) => (
-                    <div className="book" key={book.primary_isbn10}>
-                        <div className="imgBox">
-                            <img src={book.book_image} />
-                        </div>
-                        <div className="rank">{book.rank}</div>
-                        <div className="clip" />
-                        <div className="bookName">{book.title}</div>
-                        <div className="author">{book.author}</div>
-                        <div className="buyBtn">
-                            <Link href={book.buy_links[5].url}>
-                                <a target="_blank">Buy now &rarr;</a>
-                            </Link>
-                        </div>
-                    </div>
-                ))}
+  const { books } = bookList;
+  return (
+    <div className="bookList">
+      <div className="title">{bookList.display_name} BOOKS</div>
+      <div className="booksWrap">
+        {books.map((book) => (
+          <div className="book" key={book.primary_isbn10}>
+            <div className="imgBox">
+              <img src={book.book_image} />
             </div>
-            <style jsx>
-                {`
-                    .booksWrap {
-                        display: grid;
-                        grid-template-columns: repeat(4, 1fr);
-                        width: 80%;
-                        gap: 50px 30px;
-                        padding: 20px;
-                        margin: 0 auto;
-                        border-left: 1px solid #449745;
-                        border-right: 1px solid #449745;
-                    }
-                    .book {
-                        display: flex;
-                        flex-direction: column;
-                        justify-content: flex-start;
-                        align-items: center;
-                        position: relative;
-                        box-shadow: 0 0 1rem -3px hsla(0, 0%, 0%, 0.2), inset 0 0 1rem -3px hsla(0, 0%, 0%, 0.1);
-                        padding-bottom: 10px;
-                    }
-                    .imgBox {
-                        background-color: #065a07;
-                        width: 100%;
-                        height: 280px;
-                        display: flex;
-                        align-items: center;
-                        justify-content: center;
-                    }
-                    .imgBox img {
-                        width: 60%;
-                        max-height: 200px;
-                        object-fit: contain;
-                    }
+            <div className="rank">{book.rank}</div>
+            <div className="clip" />
+            <div className="bookName">{book.title}</div>
+            <div className="author">{book.author}</div>
+            <div className="buyBtn">
+              <Link href={book.buy_links[5].url}>
+                <a target="_blank">Buy now &rarr;</a>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+      <style jsx>
+        {`
+          .booksWrap {
+            display: grid;
+            grid-template-columns: repeat(4, 1fr);
+            width: 80%;
+            gap: 50px 30px;
+            padding: 20px;
+            margin: 0 auto;
+            border-left: 1px solid #449745;
+            border-right: 1px solid #449745;
+          }
+          .book {
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-start;
+            align-items: center;
+            position: relative;
+            box-shadow: 0 0 1rem -3px hsla(0, 0%, 0%, 0.2),
+              inset 0 0 1rem -3px hsla(0, 0%, 0%, 0.1);
+            padding-bottom: 10px;
+          }
+          .imgBox {
+            background-color: #065a07;
+            width: 100%;
+            height: 280px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+          .imgBox img {
+            width: 60%;
+            max-height: 200px;
+            object-fit: contain;
+          }
 
-                    .bookName {
-                        font-size: 20px;
-                        padding: 8px 5px;
-                        font-weight: bold;
-                        text-align: center;
-                        color: #063f07;
-                    }
-                    .author {
-                        text-align: center;
-                        padding: 0 5px;
-                        font-size: 14px;
-                        padding: 5px 0;
-                    }
-                    .buyBtn {
-                        font-size: 16px;
-                        border: 1px solid #84b284;
-                        border-radius: 30px;
-                        padding: 5px 15px 7px;
-                        margin: 10px 0 5px;
-                        cursor: pointer;
-                        &:hover {
-                            background-color: #063f07;
-                            color: #ffffff;
-                            border: 1px solid #063f07;
-                        }
-                    }
-                    .rank {
-                        color: tomato;
-                        font-family: 'Heebo', sans-serif;
-                        font-size: 28px;
-                        font-weight: bold;
-                        position: absolute;
-                        top: -5px;
-                        right: 7px;
-                        z-index: 99;
-                        display: flex;
-                        align-items: center;
-                        justify-content: center;
-                    }
-                    .clip {
-                        position: absolute;
-                        top: 0;
-                        right: 0;
-                        width: 0;
-                        height: 0;
-                        transform: rotate(-180deg);
-                        border-bottom: 30px solid #ffffff;
-                        border-top: 30px solid transparent;
-                        border-left: 40px solid #ffffff;
-                        border-right: 40px solid transparent;
-                    }
-                `}
-            </style>
-        </div>
-    );
+          .bookName {
+            font-size: 20px;
+            padding: 8px 5px;
+            font-weight: bold;
+            text-align: center;
+            color: #063f07;
+          }
+          .author {
+            text-align: center;
+            padding: 0 5px;
+            font-size: 14px;
+            padding: 5px 0;
+          }
+          .buyBtn {
+            font-size: 16px;
+            border: 1px solid #84b284;
+            border-radius: 30px;
+            padding: 5px 15px;
+            margin: 10px 0 5px;
+            cursor: pointer;
+            &:hover {
+              background-color: #063f07;
+              color: #ffffff;
+              border: 1px solid #063f07;
+            }
+          }
+          .rank {
+            color: tomato;
+            font-family: "Heebo", sans-serif;
+            font-size: 28px;
+            font-weight: bold;
+            position: absolute;
+            top: 4px;
+            right: 7px;
+            z-index: 99;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+          .clip {
+            position: absolute;
+            top: 0;
+            right: 0;
+            width: 0;
+            height: 0;
+            transform: rotate(-180deg);
+            border-bottom: 30px solid #ffffff;
+            border-top: 30px solid transparent;
+            border-left: 40px solid #ffffff;
+            border-right: 40px solid transparent;
+          }
+        `}
+      </style>
+    </div>
+  );
 }
 export async function getServerSideProps({ params: { id } }) {
-    const { results: bookList } = await (await fetch(`http://localhost:3000/api/books/${id}`)).json();
+  const { results: bookList } = await (
+    await fetch(`http://localhost:3000/api/books/${id}`)
+  ).json();
 
-    return {
-        props: {
-            id,
-            bookList,
-        },
-    };
+  return {
+    props: {
+      id,
+      bookList,
+    },
+  };
 }
