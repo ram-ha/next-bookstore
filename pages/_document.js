17276b0d2717e34dd7ba008023579c055a2d515e
hram <dlgkfka2616@gmail.com>
2023-10-15 21:18:23 +0900

    font & error edit

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Cormorant+Infant:wght@300;600&display=swap');
+      </style>
+
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,18 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
 
 export default function IndexPage({ books }) {
-  const router = useRouter();
-  const onClick = (list_name, list_name_encoded) => {
-    router.push(`/list/${list_name}/${list_name_encoded}`)
-  }
   return (
     <div className="container">
       <h2 className="title">THE NEW YORK TIMES BEST SELLER EXPLORER</h2>
       <div className="list">
-        {books.map(book => (
-          <div className="listName border button"
-            onClick={() => onClick(book.list_name, book.list_name_encoded)}
-            key={book.list_name_encoded}>
-            <Link href={`/list/${book.list_name}`} legacyBehavior>
-              {book.display_name} &rarr;
+        {books.map((book) => (
+          <div className="listName border button">
+            <Link href={`/list/${book.list_name}`} key={book.list_name_encoded}>
+              <span>{book.display_name} &rarr;</span>
             </Link>
           </div>
         ))}
+        {console.log(books.filter((book) => book.updated === "WEEKLY"))}
       </div>
       <style jsx>{`
         .list {
@@ -26,6 +20,7 @@ export default function IndexPage({ books }) {
         }
         .listName {
           display: inline-block;
+          cursor: pointer;
           padding: 10px 20px;
           margin: 10px 15px 10px 0;
         }
@@ -38,7 +33,6 @@ export async function getServerSideProps() {
   const { results: books } = await (
     await fetch(`http://localhost:3000/api/books`)
   ).json();
-  console.log(books)
 
   return {
     props: { books },
